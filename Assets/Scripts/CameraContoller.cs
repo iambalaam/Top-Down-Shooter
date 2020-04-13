09c6ddbf1a4e26dfd7d7acfd654951dedc2081a7
Guy <guy.balaam@gmail.com>
2020-04-13 14:49:34 +0100

    Adding follow camera

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraContoller : MonoBehaviour
+{
+    private GameObject player;
+    [SerializeField] [Range(0f, 10f)] private float cameraSpeed = 5f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Vector3 cameraTarget = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, cameraTarget, cameraSpeed * Time.deltaTime);
+    }
+}
